998e39812500a6d43910d096f59d5955885edb5d
Durgesh Pandey <heartbeatanna1608@gmail.com>
2022-11-01 03:57:23 +0000

    [Progress] change codebase to run simple http server

--- main.go
+++ main.go
@@ -2,16 +2,36 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
-func main() {
+func handler(w http.ResponseWriter, r *http.Request) {
 	rdg := RandomDataGenerator{}
 	rdg.Init()
 	reqId := rdg.RandomRequestId(10)
 	data := rdg.Generate()
+	response := ""
 
-	fmt.Println("---------------------------")
-	fmt.Println("Request ID: ", reqId)
-	fmt.Println("Data: ", data)
-	fmt.Println("---------------------------")
+	response += fmt.Sprintln("---------------------------")
+	response += fmt.Sprintln("Request ID: ", reqId)
+	response += fmt.Sprintln("Data: ", data)
+	response += fmt.Sprintln("---------------------------")
+
+	w.Header().Add("Content-Type", "text/plain")
+	w.Write([]byte(response))
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+
+	done := make(chan bool)
+
+	go func() {
+		if err := http.ListenAndServe(":8080", nil); err != nil {
+			done <- true
+		}
+	}()
+
+	<-done
+	fmt.Println("Server shutting down")
 }
